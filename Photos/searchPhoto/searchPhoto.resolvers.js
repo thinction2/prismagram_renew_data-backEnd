04b8f6c1952fd0aa6743fcd01100a818e417d032
LeeJinSeok <dudwlsehdwk@nate.com>
2021-07-28 17:36:54 +0900

    2021.07.28 some bugs fixed & create Query me

--- Photos/searchPhoto/searchPhoto.resolvers.js
+++ Photos/searchPhoto/searchPhoto.resolvers.js
@@ -3,6 +3,6 @@ import client from "../../client";
 export default {
   Query: {
     searchPhotos: (_, { keyword }) =>
-      client.photo.findMany({ where: { caption: { startsWith: keyword } } }),
+      client.photo.findMany({ where: { caption: { contains: keyword } } }),
   },
 };
